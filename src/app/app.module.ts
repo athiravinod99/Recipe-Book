67a6bd15d6b74b139ba3ac9019a530641a45acfb
athiravinod99 <athiravinod.mec@gmail.com>
2023-02-15 11:39:38 +0530

    lazy loading implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AuthComponent } from './auth/auth.component';
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    AuthComponent,
-  ],
+    HeaderComponent
+    ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule,
-    ShoppingListModule,
     SharedModule,
-    CoreModule
+    CoreModule,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -6,7 +6,6 @@ import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.compo
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RouterModule } from "@angular/router";
-import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RecipeRoutingModule } from "./recipes-routing.module";
 import { SharedModule } from "../shared/shared.module";
@@ -24,7 +23,7 @@ import { SharedModule } from "../shared/shared.module";
         RouterModule,
         ReactiveFormsModule,
         RecipeRoutingModule,
-        SharedModule
+        SharedModule,
     ]
 })
 export class RecipesModule {
